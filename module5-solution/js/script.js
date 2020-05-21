9fe146858e46c0a90a44de09cc98e24679505ccb
Rajat947 <choudharyrajat1311@gmail.com>
2020-05-21 20:16:24 +0530

    sol

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -125,6 +125,7 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
       insertHtml('#main-content',homeHtmlToInsertIntoMainPage);
+      
     }
     ,false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
