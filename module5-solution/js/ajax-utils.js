d75b9674479828fc44cbdad8e3a56e5b01de4f6d
Rajat947 <choudharyrajat1311@gmail.com>
2020-05-21 18:53:10 +0530

    solution of module 5

--- module5-solution/js/ajax-utils.js
+++ module5-solution/js/ajax-utils.js
@@ -6,10 +6,10 @@ var ajaxUtils = {};
 
 // Returns an HTTP request object
 function getRequestObject() {
-  if (global.XMLHttpRequest) {
+  if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
   } 
-  else if (global.ActiveXObject) {
+  else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
   } 
